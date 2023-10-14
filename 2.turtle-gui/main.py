c07ff38321fe7c6972dccdd4952c0d3cceca1d3c
ahmedawwan <ahmedsaeed0973@gmail.com>
2023-10-14 13:50:16 +0500

    sqaure task

--- 2.turtle-gui/main.py
+++ 2.turtle-gui/main.py
@@ -2,8 +2,13 @@
 Turtle Program
 """
 from turtle import Turtle, Screen
+from turtle_square import draw_square
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+draw_square(turtle=timmy_the_turtle)
+
 
 SCREEN = Screen()
 
---
+++ 2.turtle-gui/turtle_square.py
@@ -0,0 +1,5 @@
+def draw_square(turtle):
+    for _ in range(4):
+        turtle.right(90)
+        turtle.forward(100)
+
